d2750adb63845c8e2f387a68ccebc80c1968d4ba
sfffaaa <sfffaaa@gmail.com>
2026-04-17 07:47:03 +0200

    fix: use OZ Ownable and rename to mUSDC

--- contracts/src/MockUSDC.sol
+++ contracts/src/MockUSDC.sol
@@ -2,20 +2,16 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MockUSDC is ERC20 {
-    address public owner;
-
-    constructor() ERC20("USD Coin", "USDC") {
-        owner = msg.sender;
-    }
+contract MockUSDC is ERC20, Ownable {
+    constructor() ERC20("Mock USD Coin", "mUSDC") Ownable(msg.sender) {}
 
     function decimals() public pure override returns (uint8) {
         return 6;
     }
 
-    function mint(address to, uint256 amount) external {
-        require(msg.sender == owner, "not owner");
+    function mint(address to, uint256 amount) external onlyOwner {
         _mint(to, amount);
     }
 }
