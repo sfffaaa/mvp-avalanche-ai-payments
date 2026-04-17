a805077d8ecce723e083be1f39b1f3b555ce8fc6
sfffaaa <sfffaaa@gmail.com>
2026-04-17 07:28:26 +0200

    chore: project scaffold

---
+++ jest.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  preset: "ts-jest/presets/default-esm",
+  extensionsToTreatAsEsm: [".ts"],
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+  transform: {
+    "^.+\\.tsx?$": ["ts-jest", { useESM: true }],
+  },
+  testEnvironment: "node",
+};
+
+export default config;
